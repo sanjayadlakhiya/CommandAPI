bb3f9d18abd2d1dd6276bfc2810858857def5b56
sanjayadlakhiya2 <sanjay.adlakhiya@compunneldigital.com>
2023-01-08 23:56:33 +0530

    Added Command Model to API Project

---
+++ src/CommandApi/Controllers/CommandsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"this", "is", "hard", "coded" };
+        }
+    }
+}
---
+++ src/CommandApi/Models/Command.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandApi.Models
+{
+    public class Command
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string HowTo { get; set; }
+        [Required]
+        public string Platform { get; set; }
+        [Required]
+        public string CommandLine { get; set; }
+    }
+}
